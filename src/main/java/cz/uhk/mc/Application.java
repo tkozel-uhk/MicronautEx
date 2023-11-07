5c80f45212a34e67f710583d9d8b05b21ee3771f
Tomas Kozel <tkozel-uhk>
2023-11-07 20:08:55 +0000

    Initial commit

---
+++ src/main/java/cz/uhk/mc/Application.java
@@ -0,0 +1,10 @@
+package cz.uhk.mc;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
